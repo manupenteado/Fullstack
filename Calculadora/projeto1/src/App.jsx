33ce005c16e5a950c1558901d5eb661b44d09747
Manu <penteadoemanuele@gmail.com>
2025-03-27 09:02:03 -0300

    vercel test

--- Calculadora/projeto1/src/App.jsx
+++ Calculadora/projeto1/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import DateInput from './Components/DateInput';
 import Result from './Components/Result';
 import Button from './Components/Button';
-import { isValidDate } from './utils/ValidDate';
+import { isValidDate } from './Utils/JS ValidDate';
 
 function App() {
   const [day, setDay] = useState('');
